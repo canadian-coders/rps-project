f100db6c6ed5dd1a5c64958cdeacfd6cda78855c
cerafinn <74612014+cerafinn@users.noreply.github.com>
2022-07-18 12:24:16 -0400

    Initial commit

---
+++ src/main/java/com/example/rpsproject/RpsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.rpsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rpsproject/RpsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rpsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
